fdda248fe6c450a050d2f8e85f10ef150149c9d3
Júlia Enriquetto de Brito <107726291+juliaenriquetto@users.noreply.github.com>
2024-10-05 15:05:07 -0300

    feat: add questions

--- src/constants.js
+++ src/constants.js
@@ -24,13 +24,53 @@ export const dialogueData = {
 
 export const quizData = [
   {
-    question: 'Quais são os gases abaixo que não participam do efeito estufa?',
-    answer: 'Gás Ozônio',
-    answers: ['Gás Carbônico', 'Gás Ozônio', 'Gás Metano', 'Gás Estufa']
+    question: 'According to NASA satellite data, what was the hottest year on record globally so far?',
+    answer: '2023',
+    answers: ['2021', '2016', ' 2010', '2023']
   },
   {
-    question: 'Quais são os animais abaixo que comem?',
-    answer: 'Todos',
-    answers: ['Nenhum', 'Apenas um', 'Apenas dois', 'Todos']
+    question: 'According to NASA records, what has been the average rate of sea level rise since 1993?',
+    answer: '4.4 mm per year',
+    answers: ['4.4 mm per year', '3.3 mm per year', '5 mm per year', '10 mm per year']
+  },
+  {
+    question: 'What is the main greenhouse gas responsible for most of global warming, according to NASA measurements?',
+    answer: 'Carbon dioxide (CO₂)',
+    answers: ['Methane (CH₄)', 'Carbon dioxide (CO₂)', 'Ozone (O₃)', 'Nitrous oxide (N₂O)']
+  },
+  {
+    question: 'Based on NASA data, what percentage of global warming is attributable to human activities, such as burning fossil fuels and deforestation?',
+    answer: 'More than 95%',
+    answers: ['Approximately 50%', 'More than 95%', 'Less than 70%', 'Around 80%']
+  },
+  {
+    question: 'According to NASA data, which region of the planet is warming at a faster rate than the global average?',
+    answer: 'The Arctic',
+    answers: ['The South Pacific', 'Antarctica', 'The Arctic', 'Western Europe']
+  },
+  {
+    question: 'According to NASA satellites, how much ice is Greenland losing every year due to global warming?',
+    answer: 'Around 300 billion tons',
+    answers: ['Around 50 billion tons', 'Around 100 billion tons', 'Around 200 billion tons', 'Around 300 billion tons']
+  },
+  {
+    question: 'What is NASAs contribution to measuring atmospheric carbon dioxide based on the Orbital Carbon Observatory (OCO-2)?',
+    answer: 'OCO-2 monitors the global distribution of CO₂, focusing on carbon sources and sinks.',
+    answers: ['NASA only measures CO₂ in urban areas.', 'OCO-2 monitors the global distribution of CO₂, focusing on carbon sources and sinks.', 'OCO-2 only measures CO₂ levels in tropical forests.', 'NASA does not measure carbon dioxide directly with OCO-2.']
+  },
+  {
+    question: 'According to NASA monitoring data, how much has the concentration of carbon dioxide in the atmosphere increased since the beginning of the industrial age?',
+    answer: '45%',
+    answers: ['10%', '30%', '45%', '60%']
+  },
+  {
+    question: 'According to NASA data, which region of the world has experienced extreme droughts more frequently in recent years?',
+    answer: 'Middle East and North Africa',
+    answers: ['North America', 'Central Europe', 'South Africa', 'Middle East and North Africa']
+  },
+  {
+    question: 'How does climate change affect biodiversity?',
+    answer: 'Reduce the habitats available to many species',
+    answers: ['They increase the number of species', 'Reduce the habitats available to many species', ' Improve soil quality', 'They have no significant impact']
   },
 ]
