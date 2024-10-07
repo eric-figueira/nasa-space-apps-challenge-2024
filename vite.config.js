501c0c4285ff98ffe215c14f6ad22bedf12f47d6
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-10-06 22:00:55 -0300

    build: try to build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path'
 
 export default defineConfig({
   base: "./",
   build: {
-    minify: "terser",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'quiz.html'),
+      },
+    },
+    minify: "terser"
   }
 })
